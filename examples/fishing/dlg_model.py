a7c439144324047abfca2c40a07434c76709cd47
David Chu <thedavidchu@gmail.com>
2023-03-15 20:57:00 -0400

    Revert model to Lenet

--- examples/fishing/dlg_model.py
+++ examples/fishing/dlg_model.py
@@ -1,28 +1,38 @@
 """
-The ResNet-18 used by Fishing paper
+The Lenet used by DLG paper
 """
-import torch.nn
 import torch.nn as nn
-import torchvision.models
-
+import torchvision
 from plato.config import Config
 
 
 class Model(nn.Module):
     def __init__(self, num_classes=Config().trainer.num_classes):
-        """Taken from breaching/cases/models/model_preparation.py"""
-        pretrained = True
         super().__init__()
-        self.body = torchvision.models.resnet18(pretrained=pretrained)
-        self.fc = torch.nn.Linear(self.body.fc.in_features, num_classes)
-        # NOTE(dchu): I'm not actually sure what this does...
-        if pretrained:
-            self.fc.weight.data = self.body.fc.weight[:num_classes]
-            self.fc.bias.data = self.body.fc.bias[:num_classes]
-        self.body.fc = self.fc
-        assert not hasattr(self, "bias")
+        act = nn.Sigmoid
+        if Config().data.datasource == 'EMNIST':
+            in_channel = 1
+            in_size = 588
+        if Config().data.datasource.startswith('CIFAR'):
+            torchvision.datasets.CIFAR10.url = "http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+            in_channel = 3
+            in_size = 768
+
+        self.body = nn.Sequential(
+            nn.Conv2d(in_channel, 12, kernel_size=5, padding=5//2, stride=2),
+            act(),
+            nn.Conv2d(12, 12, kernel_size=5, padding=5//2, stride=2),
+            act(),
+            nn.Conv2d(12, 12, kernel_size=5, padding=5//2, stride=1),
+            act(),
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(in_size, num_classes)
+        )
 
     def forward(self, x):
         out = self.body(x)
-        return out
-
+        feature = out.view(out.size(0), -1)
+        # print(out.size())
+        out = self.fc(feature)
+        return out, feature
